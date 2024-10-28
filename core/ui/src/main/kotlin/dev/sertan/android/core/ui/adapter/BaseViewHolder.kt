1b3cd22d88e7b266fce2208ee6c259daf4f7534b
Sertan Canpolat <58915038+scnplt@users.noreply.github.com>
2024-10-28 14:39:00 +0300

    🚨 Fix compiler / linter warnings

--- core/ui/src/main/kotlin/dev/sertan/android/core/ui/adapter/BaseViewHolder.kt
+++ core/ui/src/main/kotlin/dev/sertan/android/core/ui/adapter/BaseViewHolder.kt
@@ -20,7 +20,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 
 abstract class BaseViewHolder<I : BaseListItem<I>>(
-    private val binding: ViewBinding
+    private val binding: ViewBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
 
     open fun bind(item: I): Unit = Unit
